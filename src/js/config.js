ca41ac4ac01e3298e0ccd0ce79d7c3107ad7cb04
PatrickB001 <patrickborgella1@gmail.com>
2021-11-08 12:54:12 -0600

    New API Key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '7bfb8841-2441-413f-943b-84ee0dd2c93c';
+export const KEY = '63917bb1-7d7a-44f9-b41e-14f3d8382df1';
 export const MODAL_CLOSE_SEC = 2.5;
